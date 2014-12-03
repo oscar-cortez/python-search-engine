d76b08c6fe9144c08b3dd9a8dd3a84209ac33790
Oscar99 <od_cortez@galileo.edu>
2014-12-03 09:16:15 -0600

    Version Final

---
+++ proyecto5.py
@@ -0,0 +1,56 @@
+#coding: utf-8
+import re 
+import urllib, os
+print """
+	===================================
+	Bienvenido al motor de busqueda MOS
+	===================================
+	Instrucciones de uso 
+	Coloca las url de dos paginas que quieras 
+	comparar luego coloca la palabra para tu 
+	busqueda Si solo ponele recuerda que tienes
+	que quitarle la "https://" y listo disfruta del programa.
+"""
+class encontrar(object):
+        def pagina(self):
+		respuesta = 0
+		while (respuesta==0):
+			while True:
+                        	self.web=raw_input("Ingrese la primera pagina: https://")
+				self.we =raw_input("Ingrese la segunda pagina: https://")
+				self.pa =raw_input("Ingrese lo que desea buscar:")
+			
+				https = "https://"
+				req = urllib.urlopen(https + self.web)
+				r = urllib.urlopen(https + self.we)
+				b = req.read()
+				bb = r.read()
+				p = len(re.findall(self.pa,b))
+				p1 = len(re.findall(self.pa,bb))
+			
+				if p > p1:
+					print "la pagina recomendada es: ",self.web
+					print "cantidad de palabras encontradas: ", p
+					break
+				elif p == 0:
+					print "palabra no encontrada"
+				elif p1 ==0:
+					print "palabra no encrontrada"
+				else:
+					print "la pagina recomendada es: ",self.we
+					print "cantidad de palabras encontradas: ", p1
+			os.system("clear")
+			a =	raw_input  ("Desea salir? si/no ")
+			b = a.lower()
+			if b == "si":
+				respuesta = 0
+			elif b == "no":
+				break
+				respuesta = 1
+			else:
+				print "Debe ingresar una opción valida"
+
+#c = encontrar()
+p = encontrar()
+#c.encontrar()
+p.pagina()
